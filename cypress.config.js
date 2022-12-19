ed3fd38772eabe39694218d4c34dabae7f54a83b
Anna <akhrolenko85@gmail.com>
2022-12-19 22:14:03 +0300

    add cloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "odg6ex",
     baseUrl: 'http://qamid.tmweb.ru',
     "retries": 1,
     setupNodeEvents(on, config) {
