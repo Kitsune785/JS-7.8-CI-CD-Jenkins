ceabbab374899583974c60f0d3601e6c32820596
Anna <akhrolenko85@gmail.com>
2022-12-19 22:24:04 +0300

    исправление имен папки

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "odg6ex",
   e2e: {
-    projectId: "odg6ex",
     baseUrl: 'http://qamid.tmweb.ru',
-    "retries": 1,
+    //"retries": 1,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
